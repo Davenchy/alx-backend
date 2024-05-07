10b7a36aa41af601780cde889060d43f3e7ef103
Fadi Asaad <firon1222@gmail.com>
2024-05-07 08:58:53 +0300

    add task 0

---
+++ 0x01-caching/0-basic_cache.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+""" Create Basic Caching Manager class """
+
+from base_caching import BaseCaching
+from typing import Any, Optional
+
+
+class BasicCache(BaseCaching):
+    """ Basic Caching Manager """
+
+    def __init__(self):
+        """ Initialize """
+        super().__init__()
+
+    def put(self, key, item):
+        """ Add an item in the cache """
+        if key is None or item is None:
+            return
+        self.cache_data[key] = item
+
+    def get(self, key) -> Optional[Any]:
+        """ Get an item by key from cache """
+        if key is None or key not in self.cache_data:
+            return None
+        return self.cache_data[key]
