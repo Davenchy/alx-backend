a4012f845b06427a062c8447792ecca84af54670
Fadi Asaad <firon1222@gmail.com>
2024-05-15 03:54:30 +0300

    update: task 2: set render_template to task 2 files

--- 0x02-i18n/2-app.py
+++ 0x02-i18n/2-app.py
@@ -27,7 +27,7 @@ def get_local():
 @app.route('/')
 def index():
     """ get / endpoint """
-    return render_template('1-index.html')
+    return render_template('2-index.html')
 
 
 if __name__ == '__main__':
