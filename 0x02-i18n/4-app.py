853ae80243669ef2491def399503a18ca65ad47c
Fadi Asaad <firon1222@gmail.com>
2024-05-15 04:40:20 +0300

    add task 4

---
+++ 0x02-i18n/4-app.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python3
+""" Simple flask app """
+
+from flask import Flask, render_template, request
+from flask_babel import Babel
+
+
+class Config:
+    """ Flask App Config """
+
+    LANGUAGES = ["en", "fr"]
+    BABEL_DEFAULT_LOCALE = "en"
+    BABEL_DEFAULT_TIMEZONE = "UTC"
+
+
+app = Flask(__name__)
+app.config.from_object(Config)
+babel = Babel(app)
+
+
+@babel.localeselector
+def get_local():
+    """ determine best locale match for the user """
+    locale = request.args.get('locale')
+    if locale in app.config["LANGUAGES"]:
+        return locale
+    return request.accept_languages.best_match(app.config["LANGUAGES"])
+
+
+@app.route('/')
+def index():
+    """ get / endpoint """
+    return render_template('4-index.html')
+
+
+if __name__ == '__main__':
+    app.run()
