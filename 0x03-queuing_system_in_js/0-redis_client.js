d01d4ef545c3fdaf648bacaa0c6e07c332070aae
Fadi Asaad <firon1222@gmail.com>
2024-07-31 17:15:50 +0300

    add: task 1 files: 0-redis_client.js

---
+++ 0x03-queuing_system_in_js/0-redis_client.js
@@ -0,0 +1,8 @@
+import { createClient } from "redis";
+
+createClient({ url: "redis://127.0.0.1:6379" })
+  .on("connect", () => console.log("Redis client connected to the server"))
+  .on("error", (err) =>
+    console.error("Redis client not connected to the server:", err.message),
+  )
+  .connect();
