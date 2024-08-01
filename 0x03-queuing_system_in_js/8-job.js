30f20e79982fa2f468622485f20ccef4b421b660
Fadi Asaad <firon1222@gmail.com>
2024-08-01 05:58:08 +0300

    update: task 8

--- 0x03-queuing_system_in_js/8-job.js
+++ 0x03-queuing_system_in_js/8-job.js
@@ -6,19 +6,20 @@ export default function createPushNotificationsJobs(jobs, queue) {
   }
 
   for (const jobObj of jobs) {
-    const job = queue.create(channel, jobObj).save((err) => {
-      if (!err) console.log("Notification job created:", job.id);
-    });
-
-    const { id } = job;
+    const job = queue.create(channel, jobObj);
 
     job
-      .on("complete", () => console.log("Notification job %d completed", id))
+      .on("complete", () =>
+        console.log("Notification job %d completed", job.id),
+      )
       .on("failed", (err) =>
-        console.log("Notification job %d failed: %s", id, err),
+        console.log("Notification job %d failed: %s", job.id, err),
       )
       .on("progress", (progress) =>
-        console.log("Notification job %d %d%% complete", id, progress),
-      );
+        console.log("Notification job %d %d%% complete", job.id, progress),
+      )
+      .save((err) => {
+        if (!err) console.log("Notification job created:", job.id);
+      });
   }
 }
