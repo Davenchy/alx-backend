87705cfedd6389d723f9a446f2d2a9043e8886b7
Fadi Asaad <firon1222@gmail.com>
2024-08-01 03:28:36 +0300

    update: the 0-redis_client.js file to use the v2.8 redis client version

--- 0x03-queuing_system_in_js/0-redis_client.js
+++ 0x03-queuing_system_in_js/0-redis_client.js
@@ -4,5 +4,4 @@ createClient({ url: "redis://127.0.0.1:6379" })
   .on("connect", () => console.log("Redis client connected to the server"))
   .on("error", (err) =>
     console.error("Redis client not connected to the server:", err.message),
-  )
-  .connect();
+  );
